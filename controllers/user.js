f40ba1948cb93eb698cdccf5a24edb683d8c01bd
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-05 00:41:29 -0700

    adds user controller and bcrypt, jwt, and hashed password

---
+++ controllers/user.js
@@ -0,0 +1,33 @@
+const bcrypt = require('brcrypt');
+const jwt = require('jsonwebtoken');
+const UserModel = require("../models/user.js");
+
+const secret = "test";
+
+export const signup = async (req, res) => {
+  const {email, password, firstName, lastName} = req.body;
+  try {
+    const oldUser = await UserModel.findOne({email});
+    if(oldUser) {
+      return res.status(400).json({message: "User already exists"})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const result = await UserModel.create({
+      email,
+      password: hashedPassword,
+      name: `${firstName} ${lastName}`,
+    });
+
+    const token = jwt.sign({email: result.email, id: result._id}, secret, {
+      expiresIn: "24h",
+    });
+
+    res.status(201).json({result, token});
+
+  } catch(error) {
+    res.status(500).json({message: "Error"});
+    console.log(error)
+  }
+}
--- models/user.js
+++ models/user.js
@@ -5,5 +5,8 @@ const userSchema = mongoose.Schema({
   name: {type: String, required: true},
   email: {type: String, required: true},
   password: {type: String, required: false},
-  googleId: {type: String, required: true}
-})
+  googleId: {type: String, required: false},
+  id: {type: String},
+});
+
+module.exports = mongoose.model("User", userSchema)
